72efc5953c4fbbdc63cfe5ff0ebd2568010f8c65
Saad Raza <saad.dr98@gmail.com>
2022-08-13 15:22:19 +0500

    decoding Authorization headers in context function of Nexus GraphQl

--- graphql/context.ts
+++ graphql/context.ts
@@ -1,14 +1,37 @@
-import { PrismaClient } from "@prisma/client"
-import {NextApiRequest} from "next";
+import { PrismaClient, SuperUser } from "@prisma/client"
+import {NextApiRequest, NextApiResponse} from "next";
+import jwt from "jsonwebtoken";
 
 export interface Context {
     prisma: PrismaClient
+    request: NextApiRequest,
+    response: NextApiResponse,
+    user?: SuperUser
 }
 
 const prisma = new PrismaClient()
 
-export const context = ({ req } : { req: NextApiRequest}) : Context => {
-    return {
-        prisma
+export const context = ({ req, res } : { req: NextApiRequest, res: NextApiResponse}) : Context => {
+
+    let output: Context = {
+        prisma,
+        request: req,
+        response: res,
     }
+
+    decodeToken(output, req.headers.authorization)
+
+    return output
+}
+
+function decodeToken(output : Context, authorization: string | undefined){
+
+    try{
+        if (authorization && authorization.replace("Bearer ", "").length > 0) {
+            const token = authorization.split(" ")[1];
+            output.user = jwt.verify(token, process.env.JWT_SECRET || "") as SuperUser;
+        }
+    }catch(e){
+        return
+    }  
 }
