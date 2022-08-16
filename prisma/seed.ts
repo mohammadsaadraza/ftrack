09a609cd2a586fc11161a38c770b5b8dd1d58d62
Saad Raza <saad.dr98@gmail.com>
2022-08-16 20:54:18 +0500

    extra test transaction while seeding db

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,28 +4,61 @@ import bcrypt from "bcrypt"
 const prisma = new PrismaClient()
 
 async function main() {
-    const testTransaction = await prisma.transaction.create({
-        data: {
-            type: "Expense",
-            date: new Date(2022,7,8,13, 5, 0),
-            currency: "PKR",
-            description: "A test transaction",
-            amount: 10000
-        }
-    })
+  await prisma.transaction.create({
+    data: {
+      type: "Expense",
+      date: new Date(2022, 7, 8, 13, 5, 0),
+      currency: "PKR",
+      description: "A test transaction",
+      amount: 10000,
+    },
+  });
 
-    if(!process.env.SERVICE_PASSWORD || !process.env.PASS_SALT ){
-        throw new Error("Problem with Password.")
-    }
+  await prisma.transaction.create({
+    data: {
+      type: "Income",
+      date: new Date(2022, 7, 8, 13, 5, 0),
+      currency: "USD",
+      description: "A test transaction",
+      amount: 10000,
+    },
+  });
 
-    const user = await prisma.superUser.create({
-        data: {
-            type: "Admin",
-            password: await bcrypt.hash(process.env.SERVICE_PASSWORD.trim(), parseInt(process.env.PASS_SALT.trim()))
-        }
-    })
-    console.log(testTransaction)
-    console.log(user)
+  await prisma.transaction.create({
+    data: {
+      type: "Expense",
+      date: new Date(2022, 7, 8, 13, 5, 0),
+      currency: "USD",
+      description: "A test transaction",
+      amount: 10000,
+    },
+  });
+
+  await prisma.transaction.create({
+    data: {
+      type: "Income",
+      date: new Date(2022, 7, 8, 13, 5, 0),
+      currency: "PKR",
+      description: "A test transaction",
+      amount: 10000,
+    },
+  });
+
+  if (!process.env.SERVICE_PASSWORD || !process.env.PASS_SALT) {
+    throw new Error("Problem with Password.");
+  }
+
+  const user = await prisma.superUser.create({
+    data: {
+      type: "Admin",
+      password: await bcrypt.hash(
+        process.env.SERVICE_PASSWORD.trim(),
+        parseInt(process.env.PASS_SALT.trim())
+      ),
+    },
+  });
+
+  console.log(user);
 }
 
 main()
