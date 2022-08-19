ed3750816277c987468e5a31ba76c90d1559f36b
Saad Raza <saad.dr98@gmail.com>
2022-08-19 23:10:08 +0500

    configured apollo client

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,11 +4,15 @@ import '../styles/globals.css'
 import { Provider } from 'react-redux'
 
 import store from '../store'
+import apolloClient from "../hooks/apollo";
+import { ApolloProvider } from "@apollo/client";
 
 function MyApp({ Component, pageProps} : AppProps) {
   return (
     <Provider store={store}>
-      <Component {...pageProps} />
+      <ApolloProvider client={apolloClient}>
+        <Component {...pageProps} />
+      </ApolloProvider>
     </Provider>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,11 @@
+import { useApolloClient } from "@apollo/client"
+import { useStore } from "react-redux"
+
 
 export default function Home() {
+  const client = useApolloClient()
+  const store = useStore()
+  console.log(client)
+  console.log(store)
   return <div>Index Page</div>
 }
